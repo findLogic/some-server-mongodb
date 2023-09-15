446f40b02e66ae762d7c24b5dfb2c24037581726
findLogic <fun-elco@list.ru>
2023-09-15 12:48:34 +0700

    pre-commit hooks, lint-staged, docker-mongo, prettier and eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier',
   ],
   root: true,
   env: {
@@ -17,9 +18,18 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    indent: [
+      2,
+      4,
+      {
+        SwitchCase: 1,
+        ignoredNodes: ['PropertyDefinition'],
+      },
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['warn', { endOfLine: 'auto' }],
   },
 };
